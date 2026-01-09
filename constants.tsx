b157fae0d0d8e98e12b2033514523d78ed21ff12
bokeumche <bokeumche@gmail.com>
2026-01-09 23:39:34 +0900

    feat: Update metadata and project descriptions
    Reflects changes in application name and refines descriptions for portfolio items to be more concise and accurate.

--- constants.tsx
+++ constants.tsx
@@ -34,18 +34,18 @@ export const BRANDING_DATA: PortfolioItem[] = [
 ];
 
 export const PLANNING_DATA: PortfolioItem[] = [
-  { id: 1, title: '코리빙 브랜드 로컬스티치 〈오리지널스 통영〉', description: '전통 장인과 신진 창작자를 위한 교류 프로그램 기획 및 운영', imageUrl: 'https://i.imgur.com/feGtkrZ.jpeg' },
-  { id: 2, title: '청년마을 브랜드 〈일간·주간·월간 괜찮아마을〉', description: '지역살이를 희망하는 청년을 위한 여행 프로그램 기획 및 운영', imageUrl: 'https://i.imgur.com/ZkoB89C.jpeg' },
-  { id: 3, title: '지식경험 공유 컨퍼런스 〈갑자기 섬바다〉', description: '청년 창업가를 위한 온라인 강연 및 커뮤니티 기획 및 운영', imageUrl: 'https://i.imgur.com/I5oQT13.png' },
-  { id: 4, title: '식음료 팝업 스토어 〈오묵〉', description: '목포어묵의 인지도 확대를 위한 팝업 스토어 브랜드·공간 기획 및 운영', imageUrl: 'https://i.imgur.com/1wmkCut.jpeg' },
+  { id: 1, title: '코리빙 브랜드 로컬스티치 〈오리지널스 통영〉', description: '전통 장인과 신진 창작자의 교류 프로그램 기획 및 운영', imageUrl: 'https://i.imgur.com/feGtkrZ.jpeg' },
+  { id: 2, title: '청년마을 브랜드 〈일간·주간·월간 괜찮아마을〉', description: '지역 살이를 희망하는 청년을 위한 여행 프로그램 기획 및 운영', imageUrl: 'https://i.imgur.com/ZkoB89C.jpeg' },
+  { id: 3, title: '지식경험 공유 컨퍼런스 〈갑자기 섬바다〉', description: '청년 창업가를 위한 6주 온라인 강연 기획 및 운영', imageUrl: 'https://i.imgur.com/I5oQT13.png' },
+  { id: 4, title: '식음료 팝업 스토어 〈오묵〉', description: '목포 어묵의 인지도 확대를 위한 팝업 스토어 기획 및 운영', imageUrl: 'https://i.imgur.com/1wmkCut.jpeg' },
 ];
 
 export const PROJECTS_DATA: PortfolioItem[] = [
-  { id: 1, title: '자연이 머물다 가는 곳 〈돌집〉', subtitle: 'Tumblbug Funding', description: '오브제 기획 및 펀딩·홍보(SNS) 콘텐츠 기획 및 제작', imageUrl: 'https://i.imgur.com/B2BOgfe.jpeg' },
+  { id: 1, title: '자연이 머물다 가는 곳 〈돌집〉', subtitle: 'Tumblbug Funding', description: '제품 기획 및 펀딩·홍보(SNS) 콘텐츠 기획 및 제작', imageUrl: 'https://i.imgur.com/B2BOgfe.jpeg' },
   { id: 2, title: 'BBC가 주목한 지역살이 〈괜찮아마을〉', subtitle: 'Wadiz Funding', description: '지역살이 프로그램 펀딩·홍보(SNS) 콘텐츠 기획 및 제작', imageUrl: 'https://i.imgur.com/O2MgEaP.jpeg' },
-  { id: 3, title: '원스톱 인테리어 플랫폼 〈오늘의집〉', subtitle: 'Curation Exhibition', description: '인테리어 플랫폼 큐레이션 기획전 기획·신규 브랜드 소싱', imageUrl: 'https://i.imgur.com/OrI8DfH.jpeg' },
+  { id: 3, title: '원스톱 인테리어 플랫폼 〈오늘의집〉', subtitle: 'Curation Exhibition', description: '1인 가구를 위한 큐레이션 기획전 운영·신규 브랜드 소싱', imageUrl: 'https://i.imgur.com/OrI8DfH.jpeg' },
   { id: 4, title: '레트로 스테이 〈카세트플레이어〉', subtitle: 'Space Decorating', description: '신규 스테이 브랜드·프로그램 기획 및 공간 조성', imageUrl: 'https://i.imgur.com/50Co3vR.jpeg' },
-  { id: 5, title: '〈괜찮아마을에서 괜찮아졌다〉', subtitle: 'Brand Film', description: '브랜드 필름 및 티저 영상 기획·제작', imageUrl: 'https://i.imgur.com/7ctuDUH.png' },
+  { id: 5, title: '〈괜찮아마을에서 괜찮아졌다〉', subtitle: 'Brand Film', description: '브랜드 필름 및 티저 영상 기획', imageUrl: 'https://i.imgur.com/7ctuDUH.png' },
   { id: 6, title: '아트프린트 컬렉션 〈오픈에디션〉', subtitle: 'Brand Contents', description: '신규 상업 공간 인터뷰 콘텐츠 기획 및 제작', imageUrl: 'https://i.imgur.com/ZyTYp6N.png' },
 ];
 
